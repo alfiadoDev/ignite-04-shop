cb2b40df07e3ed1e5829ef5ba3a82d1e0cee9981
Alfiado Constantino <alfiado.ngana@xit2s.com>
2024-07-19 15:50:07 +0200

    global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,7 @@
+import { globalStyle } from "@/styles/global";
 import type { AppProps } from "next/app";
+
+globalStyle();
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
